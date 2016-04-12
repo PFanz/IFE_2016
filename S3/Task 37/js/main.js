32e1626cbf72fa0ae46e6eadec295cb29ed4bcbf
Geeku <ggongpei@qq.com>
2016-04-12 14:32:18 +0800

    add element drag

--- S3/Task 37/js/main.js
+++ S3/Task 37/js/main.js
@@ -17,24 +17,30 @@
         wrap: null,
         header: null,
         status: false,
+        isDown: false,
 
         init: function(param) {
 
-            this.container = param.wrap;
             this.width = param.width || null;
-            this.wrap = this.container.querySelector('.pop-wrap');
+
+            this.container = param.wrap || null;
+            this.wrap = this.container.querySelector('.pop-wrap') || null;
             this.header = this.container.querySelector('.pop-header') || null;
-            this.type = param.type;
+            if (!this.container || !this.wrap || !this.header) {
+                console.warn('HTML格式不符');
+                return;
+            }
 
-            //this.height = param.height || 300;
+            this.type = param.type || info;
 
+            //设置提示框类型
             this.container.className += ' ' + this.type;
 
             var self = this,
                 confirm = this.container.querySelector('.confirm'),
                 cancel  = this.container.querySelector('.cancel');
 
-
+            //绑定确定按钮事件
             if(!!confirm) {
                 confirm.addEventListener('click', function() {
                     param.confirm();
@@ -42,6 +48,7 @@
                 }, false);
             }
 
+            //绑定取消按钮事件
             if(!!cancel) {
                 cancel.addEventListener('click', function() {
                     param.cancel();
@@ -49,14 +56,62 @@
                 }, false);
             }
 
+            //设置宽度
             this.width ? this.wrap.style.width = this.width : null;
 
+
+            //提示框拖动
+            var offsetX = 0,
+                offsetY = 0,
+                mL = 0,
+                mT = 0;
+            this.header.addEventListener('mousedown', _down, false);
+            this.wrap.addEventListener('mouseup', _up, false);
+            function _down (event) {
+
+                event = event || window.event;
+
+                self.isDown = true;
+
+                //获取当前的margin值
+                mL = parseInt(self.wrap.style.marginLeft || 0, 10);
+                mT = parseInt(self.wrap.style.marginTop || 0, 10);
+
+                offsetX = event.clientX - mL;
+                offsetY = event.clientY - mT;
+
+                self.header.addEventListener('mousemove', _move, false);
+            }
+            function _move(event) {
+
+                event = event || window.event;
+
+                var positionX = event.clientX - offsetX,
+                    positionY = event.clientY - offsetY;
+
+                //if(!())
+
+                self.wrap.style.marginLeft = positionX + 'px';
+                self.wrap.style.marginTop = positionY + 'px';
+
+                console.log('marginLeft:' + self.wrap.style.marginLeft + ';marginTop:' + self.wrap.style.marginTop);
+            }
+            function _up(event) {
+
+                self.isDown = false;
+                self.header.removeEventListener('mousemove', _move);
+            }
+
             this.container.addEventListener('click', function(event) {
-                event = window.event || event;
-                if(event.target.className === self.container.className) {
+
+                event = event || window.event;
+                if(event.target.className === self.container.className && !self.isDown) {
                     self.hide();
+                    self.header.removeEventListener('mousemove', _move);
                 }
+
             }, true);
+
 
             return this;
         },
@@ -65,6 +120,9 @@
 
             this.container.className += ' show';
             this.status = true;
+
+            //禁止页面滚动，不支持火狐
+            window.addEventListener('mousewheel', _stopScroll, false);
         },
 
         hide: function() {
@@ -72,9 +130,12 @@
             this.container.className = this.container.className.replace(/show/g, '').trim();
             this.wrap.style.cssText = '';
             this.status = false;
+
+            window.removeEventListener('mousewheel', _stopScroll, false);
         },
 
         toggle: function() {
+
             if(this.status) {
                 this.hide();
             } else {
@@ -82,6 +143,11 @@
             }
         }
     };
+
+    function _stopScroll(event) {
+        event = event || window.event;
+        event.preventDefault();
+    }
 
     Popuper.prototype.init.prototype = Popuper.prototype;
 
