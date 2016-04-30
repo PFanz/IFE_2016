9083f24904e9df1a8d10b77be8ac1565de6449e3
PFanz <zhongguofeifan@126.com>
2016-04-30 23:32:07 +0800

    一些常用函数：浅拷贝、深拷贝

---
+++ js/common.js
@@ -0,0 +1,25 @@
+// 浅拷贝
+function extendCopy(p) {
+  var c = {};
+　for (var i in p) {
+　　c[i] = p[i];
+　}
+　c.uber = p;
+　return c;
+}
+
+
+// 深拷贝
+//　p中赋给c
+function deepCopy(c, p) {　　　　
+	var c = c || {};
+	for (var i in p) {
+		if (p[i] && typeof p[i] === 'object') {	　　　　　　　　
+			c[i] = (p[i].constructor === Array) ? [] : {};			　　　　　　　　
+			deepCopy(p[i], c[i]);		　　　　　　
+		} else {			　　　　　　　　　
+			c[i] = p[i];			　　　　　　
+		}　　　　
+	}	　　　　
+	return c;　　
+}
